c496f325db517bb2cd5650c4f7138a81d98684f3
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-24 15:33:56 -0400

    Added offline script

---
+++ offline.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+docker-compose up
+
+mysql_docker_ready() {
+    mysqladmin ping --host=localhost --port=3306 --user=user --password=dev --protocol=tcp > /dev/null 2>&1
+}
+
+while !(mysql_docker_ready); do
+    sleep 1
+done
+
+echo "MySQL docker is ready"
